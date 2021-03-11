3f6a303b7de1fa1be71bd0df9e1fbc21ef8486fe
chen ke <joyyouxiang@163.com>
2021-03-11 22:31:21 +0800

    feat: 添加useMount和useDebounce react hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import {useState, useEffect} from "react"
 import {SearchPanel} from "./search-panel.jsx"
 import { TableList } from "./table-list.jsx"
 import qs from "qs"
-import {clearParam} from "./util"
+import {clearParam, useMount, useDebounce} from "./util"
 const apiUrl = process.env.REACT_APP_API_URL
 export const ProjectList = () => {
   const [param, setParam] = useState({
@@ -11,6 +11,7 @@ export const ProjectList = () => {
   })
   const [users, setUsers] = useState([])
   const [list, setList] = useState([])
+  const debouncedParam = useDebounce(param, 2000)
   // param改变的时候，获取项目列表，接口代码
   useEffect(() => {
     fetch(`${apiUrl}/projects?${qs.stringify(clearParam(param))}`).then(async response => {
@@ -18,14 +19,15 @@ export const ProjectList = () => {
         setList(await response.json())
       }
     })
-  }, [param])
-  useEffect(() => {
+  }, [debouncedParam])
+
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async response => {
       if(response.ok){
         setUsers(await response.json())
       }
     })
-  }, [])
+  })
   return (<div>
     <SearchPanel 
     param={param} 
--- src/screens/project-list/util.js
+++ src/screens/project-list/util.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 export const clearParam = (obj) => {
   let result = {};
   Object.keys(obj).forEach((key) => {
@@ -6,4 +7,24 @@ export const clearParam = (obj) => {
     }
   });
   return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+// 输入param，隔一段时间再才输出这个值
+// 当param改变时，使用定时器控制
+export const useDebounce = (param, time) => {
+  const [debounceValue, setDebouncesValue] = useState(param);
+  useEffect(() => {
+    let timeOut = setTimeout(() => {
+      setDebouncesValue(param);
+    }, time);
+    // 每次执行 useEffect，会先去执行return后面返回的函数
+    return () => clearTimeout(timeOut);
+  }, [param, time]);
+  return debounceValue;
 };
