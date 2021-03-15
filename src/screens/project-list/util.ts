509ce292b6380564aa2c13361e79b8561546e4c0
chen ke <joyyouxiang@163.com>
2021-03-15 22:02:06 +0800

    chore: usestate添加泛型

--- src/screens/project-list/util.ts
+++ src/screens/project-list/util.ts
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
+const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 export const clearParam = (obj: object) => {
-  let result: object = {};
+  const result = { ...obj };
   Object.keys(obj).forEach((key) => {
     // @ts-ignore
-    if (obj[key] !== "") {
+    if (isFalsy(obj[key])) {
       // @ts-ignore
-      result[key] = obj[key];
+      delete result[key];
     }
   });
   return result;
@@ -19,7 +20,7 @@ export const useMount = (callback: () => void) => {
 
 // 输入param，隔一段时间再才输出这个值
 // 当param改变时，使用定时器控制
-export const useDebounce = (param: object, time?: number) => {
+export const useDebounce = <V>(param: V, time?: number) => {
   const [debounceValue, setDebouncesValue] = useState(param);
   useEffect(() => {
     let timeOut = setTimeout(() => {
