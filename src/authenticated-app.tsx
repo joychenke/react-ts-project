f53a82c553cb41730c9c3f92f69c1a42462b01d0
chen ke <joyyouxiang@163.com>
2021-04-03 21:30:35 +0800

    chore: 删除grid布局代码

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -26,29 +26,17 @@ export const AuthenticatedApp = () => {
           <button onClick={logout}>登出</button>
         </HeaderRight>
       </Header>
-      <Nav>nav..</Nav>
       <Main>
         <ProjectList />
       </Main>
-      <Aside>aside..</Aside>
-      <Footer>footer..</Footer>
     </Container>
   );
 };
 
 const Container = styled.div`
-  display: grid;
-  grid-template-rows: 6rem 1fr 15rem;
-  grid-template-columns: 20rem 1fr 20rem;
-  grid-template-areas:
-    "header header header"
-    "nav main aside"
-    "footer footer footer";
   height: 100vh;
-  grid-gap: 10rem;
 `;
 const Header = styled.header`
-  grid-area: header;
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -59,15 +47,4 @@ const HeaderLeft = styled.div`
   align-items: center;
 `;
 const HeaderRight = styled.div``;
-const Main = styled.main`
-  grid-area: main;
-`;
-const Nav = styled.nav`
-  grid-area: nav;
-`;
-const Aside = styled.aside`
-  grid-area: aside;
-`;
-const Footer = styled.footer`
-  grid-area: footer;
-`;
+const Main = styled.main``;
