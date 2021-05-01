3c001a357e8cf50cb52a64b5820ae8765de1b8fb
chen ke <joyyouxiang@163.com>
2021-05-01 17:44:27 +0800

    feat: 添加useEditProject和pin.tsx添加标星列

--- src/screens/project-list/table-list.tsx
+++ src/screens/project-list/table-list.tsx
@@ -2,10 +2,13 @@ import { Table, TableProps } from "antd";
 import dayjs from "dayjs";
 import { User } from "./search-panel";
 import { Link } from "react-router-dom";
+import { Pin } from "components/pin";
+import { useEditProject } from "utils/project";
 export interface List {
   id: number;
   name: string;
   personId: number;
+  pin: boolean;
   organization: string;
   created: number;
 }
@@ -17,9 +20,24 @@ interface TableListProps extends TableProps<List> {
 // 剩下的键值都放在props里, 如果用类型别名定义props的类型则如下PropsType：
 // type PropsType = Omit<TableListProps, 'users'>
 export const TableList = ({ users, ...props }: TableListProps) => {
+  // useEditProject是react hook，只能在顶层调用，而mutate是纯函数，不受此规则限制
+  const { mutate } = useEditProject();
+  // 函数式编程，point free的写法
+  const pinProject = (id: number) => (pin: boolean) => mutate({ id, pin });
   return (
     <Table
       columns={[
+        {
+          title: <Pin checked={true} disabled={true}></Pin>,
+          render(value, project) {
+            return (
+              <Pin
+                checked={project.pin}
+                onCheckedChange={pinProject(project.id)}
+              />
+            );
+          },
+        },
         {
           title: "名称",
           dataIndex: "name",
