79de2c25543c683ed6d66bfb7bda497e6f02cf25
chen ke <joyyouxiang@163.com>
2021-05-30 16:15:14 +0800

    feat: 用react-query实现optimistic update

--- src/utils/project.ts
+++ src/utils/project.ts
@@ -1,5 +1,6 @@
 import { useMutation, useQuery, useQueryClient } from "react-query";
 import { List } from "screens/project-list/table-list";
+import { useProjectsSearchParams } from "screens/project-list/util";
 import { useHttp } from "./http";
 
 // 获取列表信息的hook
@@ -14,6 +15,8 @@ export const useProject = (param?: Partial<List>) => {
 export const useEditProject = () => {
   const client = useHttp();
   const queryClient = useQueryClient();
+  const [searchParams] = useProjectsSearchParams();
+  const queryKey = ["projects", searchParams];
   return useMutation(
     (params: Partial<List>) =>
       client(`projects/${params.id}`, {
@@ -23,6 +26,24 @@ export const useEditProject = () => {
     {
       // 上面的PATCH方法执行完后，去执行key包含projects的 useQuery
       onSuccess: () => queryClient.invalidateQueries("projects"),
+      async onMutate(target: Partial<List>) {
+        const previousItems = queryClient.getQueryData(queryKey);
+        queryClient.setQueryData(queryKey, (old?: List[]) => {
+          return (
+            old?.map((project) =>
+              project.id === target.id ? { ...project, ...target } : project
+            ) || []
+          );
+        });
+        return { previousItems };
+      },
+      // 回滚
+      onError(error, newItem, context) {
+        queryClient.setQueryData(
+          queryKey,
+          (context as { previousItems: List[] }).previousItems
+        );
+      },
     }
   );
 };
