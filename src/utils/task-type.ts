7d1313feb6a538e751c8365ceece2fffe903cc66
chen ke <joyyouxiang@163.com>
2021-06-16 23:16:07 +0800

    feat: 加图标对样式优化

---
+++ src/utils/task-type.ts
@@ -0,0 +1,9 @@
+// 获取task type列表
+import { useQuery } from "react-query";
+import { TaskType } from "types/task-type";
+import { useHttp } from "./http";
+
+export const useTaskTypes = () => {
+  const client = useHttp();
+  return useQuery<TaskType[]>(["taskTypes"], () => client("taskTypes"));
+};
