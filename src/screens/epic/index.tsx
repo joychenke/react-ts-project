bcbd02050d9f85fa4bf7109d7dbf6ac43c735033
chen ke <joyyouxiang@163.com>
2021-07-11 20:48:17 +0800

    feat: 任务组页面开发

--- src/screens/epic/index.tsx
+++ src/screens/epic/index.tsx
@@ -1,3 +1,86 @@
+import { Button, List, Modal } from "antd";
+import { Row, ScreenContainer } from "components/lib";
+import dayjs from "dayjs";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useProjectInUrl } from "screens/kanban/util";
+import { Epic } from "types/epic";
+import { useDeleteEpic, useEpics } from "utils/epic";
+import { useTasks } from "utils/task";
+import { CreateEpic } from "./create-epic";
+import { useEpicSearchParams, useEpicsQueryKey } from "./util";
+
 export const EpicScreen = () => {
-  return <h1>epic</h1>;
+  const { data: currentProject } = useProjectInUrl();
+  const { data: epics } = useEpics(useEpicSearchParams());
+  const { data: tasks } = useTasks({ projectId: currentProject?.id });
+  const { mutate: deleteEpic } = useDeleteEpic(useEpicsQueryKey());
+  const [epicCreateOpen, setEpicCreateOpen] = useState(false);
+
+  const confirmDeleteProject = (epic: Epic) => {
+    Modal.confirm({
+      title: `确定要删除向剧组：${epic.name}？`,
+      content: "点击确定删除",
+      okText: "确定",
+      onOk() {
+        deleteEpic({ id: epic.id });
+      },
+    });
+  };
+
+  return (
+    <ScreenContainer>
+      <Row between={true}>
+        <h1>{currentProject?.name}-任务组</h1>
+        <Button onClick={() => setEpicCreateOpen(true)} type={"link"}>
+          创建任务组
+        </Button>
+      </Row>
+      <List
+        style={{ overflowY: "scroll" }}
+        dataSource={epics}
+        itemLayout={"vertical"}
+        renderItem={(epic) => (
+          <List.Item>
+            <List.Item.Meta
+              title={
+                <Row between={true}>
+                  <span>{epic.name}</span>
+                  <Button
+                    type={"link"}
+                    onClick={() => confirmDeleteProject(epic)}
+                  >
+                    删除
+                  </Button>
+                </Row>
+              }
+              description={
+                <div>
+                  <div>开始时间: {dayjs(epic.start).format("YYYY-MM-DD")}</div>
+                  <div>结束时间: {dayjs(epic.end).format("YYYY-MM-DD")}</div>
+                </div>
+              }
+            ></List.Item.Meta>
+            <div>
+              {tasks
+                ?.filter((task) => task.epicId === epic.id)
+                .map((task) => (
+                  <Link
+                    to={`/projects/${currentProject?.id}/kanban?editingTaskId=${task.id}`}
+                    key={task.id}
+                    style={{ display: "block" }}
+                  >
+                    {task.name}
+                  </Link>
+                ))}
+            </div>
+          </List.Item>
+        )}
+      ></List>
+      <CreateEpic
+        onClose={() => setEpicCreateOpen(false)}
+        visible={epicCreateOpen}
+      ></CreateEpic>
+    </ScreenContainer>
+  );
 };
---
+++ src/types/epic.ts
@@ -0,0 +1,9 @@
+export interface Epic {
+  name: string;
+  id: number;
+  projectId: number;
+  // 开始时间
+  start: number;
+  // 结束时间
+  end: number;
+}
